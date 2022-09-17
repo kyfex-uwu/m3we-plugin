79489ae8bf2c08bcf7a788fdf37d07b649a4ac54
kyfex_uwu <81386004+kyfex-uwu@users.noreply.github.com>
2022-09-17 12:38:12 -0700

    just doing some filler stuff to test if it wrkds

--- m3we-plugin.js
+++ m3we-plugin.js
@@ -2,11 +2,29 @@
     var button;
     
     Plugin.register("m3we_plugin", {
-      title: "M3WE Plugin",
-      author: "KYFEX",
-      description: "Tool for making M3WE asset creation even easier",
-      icon: "build_circle",
-      version: "0.0.0",
-      variant: "both"
+        title: "M3WE Plugin",
+        author: "KYFEX",
+        description: "Tool for making M3WE asset creation even easier",
+        icon: "build_circle",
+        version: "0.0.0",
+        variant: "both",
+        onload() {
+            button = new Action('randomize_height', {
+                name: 'Randomize Height',
+                description: 'Randomize the height of all selected elements',
+                icon: 'bar_chart',
+                click: function() {
+                    Undo.initEdit({elements: Cube.selected});
+                    Cube.selected.forEach(cube => {
+                        cube.to[1] = cube.from[0] + Math.floor(Math.random()*8);
+                    });
+                    Undo.finishEdit('randomize cube height');
+                }
+            });
+            MenuBar.addAction(button, 'filter');
+        },
+        onunload() {
+            button.delete();
+        }
   });
 })();
