ffddc4583c98680375639f65e636ebaa8b9099a0
kyfex_uwu <81386004+kyfex-uwu@users.noreply.github.com>
2022-09-17 11:00:57 -0700

    Create m3we-plugin.js

---
+++ m3we-plugin.js
@@ -0,0 +1,12 @@
+(function() {
+    var button;
+    
+    Plugin.register("m3we_plugin", {
+      title: "M3WE Plugin",
+      author: "KYFEX",
+      description: "Tool for making M3WE asset creation even easier",
+      icon: "build_circle",
+      version: "0.0.0",
+      variant: "both"
+  });
+})();
